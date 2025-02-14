ba8e84f86e209f72cf2b16ab7cb4c4692f59f4c1
David Murdoch <dsmurdoch357@gmail.com>
2025-02-14 15:32:17 +0000

    ci(analyze): analyze and report bundle size

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,16 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
 };
 
-export default nextConfig;
+export default () => {
+  const plugins = [withBundleAnalyzer({ enabled: process.env.ANALYZE })];
+
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+
+  return config;
+};
