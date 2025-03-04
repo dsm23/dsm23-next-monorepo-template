09238b74d062c8f8993277983501c8990cc7e425
David Murdoch <dsmurdoch357@gmail.com>
2025-03-04 00:17:44 +0000

    ci(docker): add Dockerfile and pipeline

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
 
+  output: "standalone",
   async rewrites() {
     return await [
       { source: "/healthz", destination: "/api/health" },
