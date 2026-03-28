578192feab1eb5235b026f0c2a85994ce17ca709
David Murdoch <dsmurdoch357@gmail.com>
2026-03-28 17:32:09 +0000

    refactor(lint): finish migration to from eslint to oxlint

--- apps/storybook/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import { config } from "@repo/eslint-config/next";
-import { defineConfig } from "eslint/config";
-
-/** @type {import("eslint").Linter.Config} */
-export default defineConfig(config);
--- packages/eslint-config/storybook.js
+++
@@ -1,9 +0,0 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-import { defineConfig } from "eslint/config";
-import { config as baseConfig } from "./base.js";
-
-export const config = defineConfig(
-  baseConfig,
-  storybook.configs["flat/recommended"],
-);
